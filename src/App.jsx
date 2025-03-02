5f0d13150cfb794dfdcd30c0ae95a0572f59a684
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-03-02 18:42:18 -0300

    Commit Inicial.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Link, Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <nav id="navbar">
+        <h2>
+          <Link to="/">MoviesLib</Link>
+        </h2>
+          <Link to="/movie/1">Movie</Link>
+          <Link to="/search">Search</Link>
+      </nav>
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
