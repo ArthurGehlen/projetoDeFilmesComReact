a776e63a4a2ecede020e60fd4a9f9996bf73ccbd
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-03-04 21:50:03 -0300

    Navbar feita, começo da estilização e começando a trabalhar com API.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from 'react'
+
+const movies_url = import.meta.env.VITE_API
+const api_key = import.meta.env.VITE_API_KEY
+
 function Home() {
+    const [topMovies, setTopMovies] = useState([])
+
+    const get_top_rated_movies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+
+        setTopMovies(data.results)
+    }
+
+    useEffect(() => {
+        const top_rated_url = `${movies_url}top_rated?${api_key}`
+
+        get_top_rated_movies(top_rated_url)
+    }, [])
+
     return (
         <div>
-            Home
+            {topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
         </div>
     );
 }
