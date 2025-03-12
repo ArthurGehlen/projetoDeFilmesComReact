8900c29ccc890654320b798f76e698ac9cbde976
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-03-11 21:58:45 -0300

    Adicionando a função de busca, e, fazendo a página de search.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
+import { useState } from 'react'
 
 import './Navbar.css'
 
 function Navbar() {
+    const [search, setSearch] = useState("")
+    const navigate = useNavigate()
+
+    const handle_submit = (e) => {
+        e.preventDefault()
+        
+        if(!search) return
+
+        navigate(`/search?q=${search}`)
+
+        setSearch("")
+    }
+
     return (
         <nav id="navbar">
             <h2>
                 <Link to="/"><BiCameraMovie /> MoviesLib</Link>
             </h2>
-            <form>
+            <form onSubmit={handle_submit}>
                 <input
                     type="text"
                     placeholder="Busque um filme"
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
                     required />
                 <button type="submit">
                     <BiSearchAlt2 />
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,42 @@
+import { useState, useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom';
+import MovieCard from '../components/MovieCard'
+
+const search_url = import.meta.env.VITE_SEARCH
+const api_key = import.meta.env.VITE_API_KEY
+
 function Search() {
+    const [search_params] = useSearchParams()
+
+    const [movies, setMovies] = useState([])
+    const query = search_params.get("q")
+
+    const get_searched_movies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+
+        setMovies(data.results)
+    }
+
+    useEffect(() => {
+        const search_with_query_url = `${search_url}?${api_key}&query=${query}`
+
+        get_searched_movies(search_with_query_url)
+    }, [query])
+
     return (
-        <div>
-            Search
+        <div className='container'>
+            <h2 className='title'>
+                Resultados para: <span className='query_text'>{query}</span>
+            </h2>
+            <div className="movies_container">
+                {movies.length === 0 && <p>Carregando...</p>}
+                {movies &&
+                    movies.map((movie) =>
+                        <MovieCard movie={movie} key={movie.id} />
+                    )
+                }
+            </div>
         </div>
     );
 }
